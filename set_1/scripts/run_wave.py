8835158ea4246e358ae5d23a45c16e0d6d0f89a2
StormKoper <storm.koper@gmail.com>
2026-02-05 11:07:08 +0100

    Finished part B

--- set_1/scripts/run_wave.py
+++ set_1/scripts/run_wave.py
@@ -2,17 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ..utils.wave import Wave1D
 
-def run_wave():
-    x0 = np.linspace(0, 1, 100)
-    x0 = np.sin(2*np.pi*x0)
-
-    mywave = Wave1D(x0, 0.001, 0.01, c=1.0)
-    mywave.run(1000)
-
-    return mywave.x_arr
-
 if __name__ == "__main__":
-    x_arr = run_wave()
-    plt.imshow(x_arr, aspect='auto', cmap='viridis')
-    plt.colorbar(label='Wave Amplitude')
-    plt.show()
+    # This is part B of the assignment
+    x0 = np.linspace(0, 1, 1000)
+    x0i = np.sin(2*np.pi*x0)
+    x0ii = np.sin(5*np.pi*x0)
+    x0iii = np.where((1/5 < x0) & (x0 < 2/5), np.sin(5*np.pi*x0), 0)
+
+    for i, x0 in enumerate([x0i, x0ii, x0iii]):
+        mywave = Wave1D(x0, 0.001, 0.001, c=1.0)
+        mywave.run(1000)
+
+        plt.imshow(mywave.x_arr, aspect='auto', cmap='viridis')
+        plt.colorbar(label='Wave Amplitude')
+        plt.title('Initial Condition: ' + (i+1)*'i')
+        plt.show()
