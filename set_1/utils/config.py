1aabdb4c5b7a1833cbc872aa70b0268e02985df9
david <kraakmanuni@gmail.com>
2026-02-09 11:33:09 +0100

    Update plotting configuration to use serif font and cm fontset

--- set_1/utils/config.py
+++ set_1/utils/config.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 
 # setup global style of plotting
-plt.rcParams['mathtext.fontset'] = 'stix'
-plt.rcParams['mathtext.rm'] = 'Times New Roman'
-plt.rcParams['font.family'] ='Times New Roman'
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['font.family'] ='serif'
 plt.rcParams["figure.titlesize"] = 18
 plt.rcParams["axes.titlesize"] = 16
 plt.rcParams["axes.labelsize"] = 14 
