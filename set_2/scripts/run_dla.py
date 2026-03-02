015366f75d409ebdca41c703be5c85506c63e121
StormKoper <storm.koper@gmail.com>
2026-03-02 13:04:00 +0100

    function for DLA - DLA_MC comparison

--- set_2/scripts/run_dla.py
+++ set_2/scripts/run_dla.py
@@ -41,5 +41,38 @@ def animate_growth():
     _ = FuncAnimation(fig, update, frames=dla.x_arr.shape[-1], interval=10, blit=True)
     plt.show()
 
+def compare_dla_rw(N: int = 100, n_growth: int = 100, params: int = 10, sims: int = 10):
+    """Compare the DLA growth with random walk growth"""
+    dlas = np.ndarray((N, N, params, sims), dtype=bool)
+    mcs = np.ndarray((N, N, params, sims), dtype=bool)
+    seeds = np.random.SeedSequence(42).spawn(2*sims)
+    dla_seeds = seeds[sims:]
+    mc_seeds = seeds[:sims]
+    # DLA growth
+    for i, eta in enumerate(np.linspace(0, 1, params)):
+        for j, seed in enumerate(dla_seeds):
+            dla = DLA(N=N, eta=eta, use_jit=True, seed=seed)
+            dla.run(n_growth=n_growth)
+            dlas[..., i, j] = dla.obj_mask
+    
+    # random walk growth
+    for i, p_s in enumerate(np.linspace(0.2, 1, params)):
+        for j, seed in enumerate(mc_seeds):
+            temp = np.random.default_rng(seed).choice([0,1], size=(N, N), p=[0.5, 0.5])
+            mcs[..., i, j] = temp
+
+    avg_dla = np.mean(dlas, axis=-1)
+    avg_mc = np.mean(mcs, axis=-1)
+    msd_grid = np.mean((avg_dla[..., :, None] - avg_mc[..., None, :]) ** 2, axis=[0,1])
+    plt.figure(figsize=(8, 6), constrained_layout=True)
+    plt.imshow(msd_grid, cmap='viridis')
+    plt.colorbar(label="Mean Squared Difference")
+    plt.title("Mean Squared Difference between DLA and MC Growth")
+    plt.xticks(ticks=np.arange(params), labels=[f"{p:.2f}" for p in np.linspace(0.2, 1, params)])
+    plt.yticks(ticks=np.arange(params), labels=[f"{eta:.2f}" for eta in np.linspace(0, 1, params)])
+    plt.xlabel("MC $p_s$")
+    plt.ylabel("DLA $\\eta$")
+    plt.show()
+
 if __name__ == "__main__":
     animate_growth()
